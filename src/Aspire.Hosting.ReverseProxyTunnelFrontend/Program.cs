7053bc58215eb25302e458d40fb83321f00a6922
David Ebbo <david.ebbo@gmail.com>
2024-10-21 15:21:03 +0200

    Initial tunnel proxy logic

---
+++ src/Aspire.Hosting.ReverseProxyTunnelFrontend/Program.cs
@@ -0,0 +1,28 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var tmp = builder.Configuration.GetSection("ReverseProxy");
+Console.WriteLine("cfg:" + tmp.GetChildren().Count());
+
+builder.Services.AddReverseProxy()
+       .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddTunnelServices();
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+// Uncomment to support websocket connections
+//app.MapWebSocketTunnel("/connect-ws");
+
+// Auth can be added to this endpoint and we can restrict it to certain points
+// to avoid exteranl traffic hitting it
+app.MapHttp2Tunnel("/connect-h2");
+
+app.Run();
---
+++ src/Aspire.Hosting.ReverseProxyTunnelFrontend/Tunnel/TunnelExensions.cs
@@ -0,0 +1,110 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Net.WebSockets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Yarp.ReverseProxy.Forwarder;
+
+internal static class TunnelExensions
+{
+    public static IServiceCollection AddTunnelServices(this IServiceCollection services)
+    {
+        var tunnelFactory = new TunnelClientFactory();
+        services.AddSingleton(tunnelFactory);
+        services.AddSingleton<IForwarderHttpClientFactory>(tunnelFactory);
+        return services;
+    }
+
+    public static IEndpointConventionBuilder MapHttp2Tunnel(this IEndpointRouteBuilder routes, string path)
+    {
+        return routes.MapPost(path, static async (HttpContext context, string host, TunnelClientFactory tunnelFactory, IHostApplicationLifetime lifetime) =>
+        {
+            // HTTP/2 duplex stream
+            if (context.Request.Protocol != HttpProtocol.Http2)
+            {
+                return Results.BadRequest();
+            }
+
+            var (requests, responses) = tunnelFactory.GetConnectionChannel(host);
+
+            await requests.Reader.ReadAsync(context.RequestAborted).ConfigureAwait(false);
+
+            var stream = new DuplexHttpStream(context);
+
+            using var reg = lifetime.ApplicationStopping.Register(() => stream.Abort());
+
+            // Keep reusing this connection while, it's still open on the backend
+            while (!context.RequestAborted.IsCancellationRequested)
+            {
+                // Make this connection available for requests
+                await responses.Writer.WriteAsync(stream, context.RequestAborted).ConfigureAwait(false);
+
+                await stream.StreamCompleteTask.ConfigureAwait(false);
+
+                stream.Reset();
+            }
+
+            return EmptyResult.s_instance;
+        });
+    }
+
+    public static IEndpointConventionBuilder MapWebSocketTunnel(this IEndpointRouteBuilder routes, string path)
+    {
+        var conventionBuilder = routes.MapGet(path, static async (HttpContext context, string host, TunnelClientFactory tunnelFactory, IHostApplicationLifetime lifetime) =>
+        {
+            if (!context.WebSockets.IsWebSocketRequest)
+            {
+                return Results.BadRequest();
+            }
+
+            var (requests, responses) = tunnelFactory.GetConnectionChannel(host);
+
+            await requests.Reader.ReadAsync(context.RequestAborted).ConfigureAwait(false);
+
+            var ws = await context.WebSockets.AcceptWebSocketAsync().ConfigureAwait(false);
+
+            var stream = new WebSocketStream(ws);
+
+            // We should make this more graceful
+            using var reg = lifetime.ApplicationStopping.Register(() => stream.Abort());
+
+            // Keep reusing this connection while, it's still open on the backend
+            while (ws.State == WebSocketState.Open)
+            {
+                // Make this connection available for requests
+                await responses.Writer.WriteAsync(stream, context.RequestAborted).ConfigureAwait(false);
+
+                await stream.StreamCompleteTask.ConfigureAwait(false);
+
+                stream.Reset();
+            }
+
+            return EmptyResult.s_instance;
+        });
+
+        // Make this endpoint do websockets automagically as middleware for this specific route
+        conventionBuilder.Add(e =>
+        {
+            var sub = routes.CreateApplicationBuilder();
+            sub.UseWebSockets().Run(e.RequestDelegate!);
+            e.RequestDelegate = sub.Build();
+        });
+
+        return conventionBuilder;
+    }
+
+    // This is for .NET 6, .NET 7 has Results.Empty
+    internal sealed class EmptyResult : IResult
+    {
+        internal static readonly EmptyResult s_instance = new();
+
+        public Task ExecuteAsync(HttpContext httpContext)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
